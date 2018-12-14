42535038c37d3bbc7e0bf427303da5966733a0cf
totoro97 <totoro97@outlook.com>
2018-12-14 12:52:31 +0800

    Manual or ANTLR?

--- src/Lexer.h
+++ src/Lexer.h
@@ -1,13 +1,37 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include <fstream>
 #include <vector>
 
+enum TokenTag {
+  ID,
+  INT,
+  RESERVED,
+  OP
+};
+
 struct Token {
-  
-}
+  TokenTag tag;
+  std::string chars;
+};
 
 class Lexer {
-  Lexer();
-  std::string GetTokens(std::ifstream &in_stream, std::vector<Token> &tokens);  
+public:
+  Lexer() {}
+  virtual std::string GetTokens(std::ifstream &in_stream, std::vector<Token> &tokens) {
+    std::cout << "I am the base. " << std::endl;
+  }
+};
+
+class ManualLexer : public Lexer {
+public:
+  ManualLexer();
+  std::string GetTokens(std::ifstream &in_stream, std::vector<Token> &tokens);
+};
+
+class ANTLRLexer : public Lexer {
+public:
+  ANTLRLexer();
+  std::string GetTokens(std::ifstream &in_stream, std::vector<Token> &tokens);
 };
