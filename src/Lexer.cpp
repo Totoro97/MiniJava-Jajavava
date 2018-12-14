9fa4db28a6968b723cbc18fa082fa4783fbbc678
totoro97 <totoro97@outlook.com>
2018-12-14 13:12:10 +0800

    add rules

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,1 +1,30 @@
 #include "Lexer.h"
+#include <vector>
+#include <string>
+
+/* token rules:
+ID: [a-zA-Z_]+
+INT: -*[0-9]+
+BOOL: {
+  true, false
+}
+RESERVED: {
+  class, public, static, void, main, String, extends, int, boolean, return,
+  if, else, while, System.out.println
+  this, new
+}
+SYMBOL: {
+  , ; { } [ ] ( )...
+}
+OP: {
+  + - * && ... < >
+}
+*/
+
+ManualLexer::ManualLexer() {
+  
+}
+
+std::string ManualLexer::GetTokens(std::ifstream &in_stream, std::vector<Token> &tokens) {
+  
+}
