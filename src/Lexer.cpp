7a17981a2ca9edef636b28d3458e7dceaebe6149
totoro97 <totoro97@outlook.com>
2018-12-15 12:58:46 +0800

    add rules

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -30,6 +30,7 @@ ManualLexer::ManualLexer() {
       a->nex_[i] = b;
     }
   };
+
   // ID 
   head = new NFANode();
   auto tmp = new NFANode();
@@ -46,7 +47,55 @@ ManualLexer::ManualLexer() {
   AddEdge(head, int_tmp_0, '-', '-');
   AddEdge(head, int_tmp_1, '0', '9');
   AddEdge(int_tmp_0, int_tmp_1, '0', '9');
+  heads_.push_back(head);
+
+  // RESERVED
+  auto AddPath = [](NFANode *head, std::string path, TokenTag tag) {
+    for (uint32_t i = 0; i < path.length(); i++) {
+      if (!head->nex_[int(path[i])]) {
+        auto tmp = new NFANode();
+        head->nex_[int(path[i])] = tmp;
+      }
+      head = head->nex_[int(path[i])];
+    }
+
+    head->valid_ = true;
+    head->tag_ = tag;
+  };
+
+  head = new NFANode();
+  AddPath(head, std::string("true"), BOOL);
+  AddPath(head, std::string("false"), BOOL);
+  AddPath(head, std::string("class"), RESERVED);
+  AddPath(head, std::string("public"), RESERVED);
+  AddPath(head, std::string("static"), RESERVED);
+  AddPath(head, std::string("void"), RESERVED);
+  AddPath(head, std::string("main"), RESERVED);
+  AddPath(head, std::string("String"), RESERVED);
+  AddPath(head, std::string("extends"), RESERVED);
+  AddPath(head, std::string("int"), RESERVED);
+  AddPath(head, std::string("boolean"), RESERVED);
+  AddPath(head, std::string("return"), RESERVED);
+  AddPath(head, std::string("if"), RESERVED);
+  AddPath(head, std::string("else"), RESERVED);
+  AddPath(head, std::string("System.out.println"), RESERVED);
+  AddPath(head, std::string("this"), RESERVED);
+  AddPath(head, std::string("new"), RESERVED);
+  AddPath(head, std::string(","), SYMBOL);
+  AddPath(head, std::string(";"), SYMBOL);
+  AddPath(head, std::string("{"), SYMBOL);
+  AddPath(head, std::string("}"), SYMBOL);
+  AddPath(head, std::string("["), SYMBOL);
+  AddPath(head, std::string("]"), SYMBOL);
+  AddPath(head, std::string(")"), SYMBOL);
+  AddPath(head, std::string("("), SYMBOL);
+  AddPath(head, std::string("+"), OP);
+  AddPath(head, std::string("-"), OP);
+  AddPath(head, std::string("&&"), OP);
+  AddPath(head, std::string("*"), OP);
   
+  heads_.push_back(head);
+  return; 
 }
 
 std::string ManualLexer::GetTokens(std::ifstream &in_stream, std::vector<Token> &tokens) {
